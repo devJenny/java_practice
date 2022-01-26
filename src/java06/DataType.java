8321387b1edbff31ee444f05a0ca562e06cc71d2
devJenny <devje@naver.com>
2022-01-27 03:41:24 +0900

    Initial commit

---
+++ src/java06/DataType.java
@@ -0,0 +1,19 @@
+package java06;
+
+public class DataType {
+    public static void main(String[] args) {
+        System.out.println(6); // Number
+        System.out.println("six"); // String
+
+        System.out.println("6"); // String 6
+
+        System.out.println(6+6); // 12
+        System.out.println("6"+"6"); // 66
+
+        System.out.println(6*6); //36
+//        System.out.println("6"*"6"); // 문자열의 데이터 타입은 곱하기 연산 할 수 없음
+
+        System.out.println("1111".length()); // 4 length: 문자 길이를 알려줌
+//        System.out.println(1111.length()); // 숫자의 길이를 알려주는 연산은 없다
+    }
+}
