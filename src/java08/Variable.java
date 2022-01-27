a50909dabb00ad1f6a7d2f4864b68664a1396225
devJenny <devje@naver.com>
2022-01-27 16:31:52 +0900

    8. Variable

---
+++ src/java08/Variable.java
@@ -0,0 +1,15 @@
+package java08;
+
+public class Variable {
+    public static void main(String[] args) {
+        // 변수 = 값이 변할 수 있는 문자
+        int a = 1; // Number -> Integer ... -2, -1. -0, 1, 2 ...
+        System.out.println(a);
+
+        double b = 1.1; // real number -> double ... -2.0, -1.0, 0, 1.0, 2.0 ...
+        System.out.println(b);
+
+        String c = "Hello World";
+        System.out.println(c);
+    }
+}
