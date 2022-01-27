c17be28b210cf03591f987a8469e38c33ffc15e8
devJenny <devje@naver.com>
2022-01-27 20:38:23 +0900

    9~10 programming & debug

---
+++ src/java09/OkJavaGoInHome.java
@@ -0,0 +1,28 @@
+package java09;
+
+import org.opentutorials.iot.Elevator;
+import org.opentutorials.iot.Lighting;
+import org.opentutorials.iot.Security;
+
+public class OkJavaGoInHome {
+    public static void main(String[] args) {
+
+        String id = "JAVA APT 507";
+
+        // Elevator call
+        Elevator myElevator = new Elevator(id);
+        myElevator.callForUp(1); // 올라갈 거니까, 1층으로 보내
+
+        // Security off
+
+        Security mySecurity = new Security(id);
+        mySecurity.off(); // 보안 해제
+
+        // Light on
+        Lighting hallLamp = new Lighting(id+" / Hall Lamp");
+        hallLamp.on();
+
+        Lighting floorLamp = new Lighting(id+" / floor Lamp");
+        floorLamp.on();
+    }
+}
