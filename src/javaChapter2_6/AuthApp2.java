9b99abf7acaf87815e8e4dba88b774778798575b
devJenny <devje@naver.com>
2022-02-02 22:47:30 +0900

    2_6 operator

---
+++ src/javaChapter2_6/AuthApp2.java
@@ -0,0 +1,28 @@
+package javaChapter2_6;
+
+import java.util.Scanner;
+
+public class AuthApp2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input your id: ");
+        String id = "egoing";
+        String inputId = sc.next();
+
+        System.out.print("Input your password: ");
+        String pass = "1111";
+        String pass2 = "2222";
+        String inputPass = sc.next();
+
+        System.out.println("Hi");
+
+        boolean isRightPass = (inputPass.equals(pass) || inputPass.equals(pass2));
+        if (inputId.equals(id) && isRightPass) {
+                System.out.println("Master!");
+            } else {
+            System.out.println("Who are you?");
+        }
+    }
+}
