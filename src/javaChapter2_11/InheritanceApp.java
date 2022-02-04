d5fc9f8cc204bdd5b773aabb009cb4e6adb7a591
devJenny <devje@naver.com>
2022-02-05 00:02:04 +0900

    2_12 interface

--- src/javaChapter2_11/InheritanceApp.java
+++ src/javaChapter2_11/InheritanceApp.java
@@ -1,47 +1,39 @@
 package javaChapter2_11;
 
 class Cal {
-    public int sum(int v1, int v2) {
-        return v1 + v2;
+    int v1, v2;
+
+    Cal(int v1, int v2) {
+        System.out.println("Cal init!!");
+        this.v1 = v1;
+        this.v2 = v2;
     }
 
-    // Overloading
-    public int sum(int v1, int v2, int v3) {
-        return this.sum(v1, v2) + v3;
+    public int sum() {
+        return this.v1 + v2;
     }
 }
 
-class Cal3 extends Cal { // extends 상속, Cla에 있는 모든 메소드와 변수를 상속받게 됨
-    public int minus(int v1, int v2) {
-        return v1 - v2;
+class Cal3 extends Cal {
+
+    Cal3(int v1, int v2) {
+        super(v1, v2); // super는 부모 클래스의 생성자
+        System.out.println("Cal3 init!!");
     }
 
-    // Overriding 재정의
-    public int sum(int v1, int v2) {
-        System.out.println(("Cal3!!"));
-        return super.sum(v1, v2);
+    public int minus() {
+        return this.v1 - v2;
     }
 }
 
 public class InheritanceApp {
     public static void main(String[] args) {
-        Cal c = new Cal();
-        System.out.println(c.sum(2, 1));
-        System.out.println(c.sum(2, 1, 1));
+        Cal c = new Cal(2, 1);
+        Cal3 c3 = new Cal3(2, 1);
 
-        Cal3 c3 = new Cal3();
-        System.out.println(c3.sum(2, 1)); //c3 인스턴스에 sum 메소드를 호출해줘
-        System.out.println(c3.minus(2, 1));
-        System.out.println(c3.sum(2, 1));
+        System.out.println(c3.sum()); // 3
+        System.out.println(c3.minus()); // 1
     }
 }
 
-class Cal2 {
-    public int sum(int v1, int v2) {
-        return v1 + v2;
-    }
 
-    public int minus(int v1, int v2) {
-        return v1 - v2;
-    }
-}
---
+++ src/javaChapter2_12/InterfaceApp.java
@@ -0,0 +1,34 @@
+package javaChapter2_12;
+
+interface Calculable {
+    double PI = 3.14;
+    int sum(int v1, int v2);
+}
+
+interface Printable {
+    void print();
+}
+
+class RealCal implements Calculable, Printable { // 여러 개의 interface 구현 OK
+
+    public int sum(int v1, int v2) {
+        return v1 + v2;
+    }
+
+    public void print() {
+        System.out.println("This is RealCal!!");
+    }
+}
+
+class AdvancedPrint implements Printable {
+    public void print() {
+        System.out.println("This is RealCal!!");
+    }
+}
+
+public class InterfaceApp {
+    public static void main(String[] args) {
+        Printable c = new AdvancedPrint();
+        c.print();
+    }
+}
